b3bcc3a265c6088c830ddfe049d68329b8165275
Chelsea Bridson <bridsonc@msu.edu>
2016-11-25 14:02:51 -0500

    touch up layout - client/app/components/Layout.jsx   rename classes - client/app/components/Logo.jsx   added chel to logo - client/app/components/Routes.jsx   added resume page - client/app/styles.css   added new colors - client/portfolio/index.js   use grid system - client/portfolio/styles.css   change shadowing on portfolio entries - client/resume/index.js   new component to display resume TODO: add link to PDF - client/resume/styles.css   center resume content - public/cv.pdf   new file - resume

--- client/app/components/Routes.jsx
+++ client/app/components/Routes.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import React from 'react'
+import { Router, Route, Link, hashHistory } from 'react-router'
 import Layout from '../components/Layout'
-import Portfolio from '../../portfolio';
+import Portfolio from '../../portfolio'
+import Resume from '../../resume'
 
 const Routes = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Layout} >
       <Route path='portfolio' component={Portfolio} />
+      <Route path='resume' component={Resume} />
     </Route>
   </Router>
 )
