17060124925125f130f94ae269e0a8e622d2fd87
Chelsea Bridson <bridsonc@msu.edu>
2016-11-25 15:28:19 -0500

    Added contact page - app/components/Routes.jsx   Added new contact page - contact/index.js   Added a simple link to my email address

--- client/app/components/Routes.jsx
+++ client/app/components/Routes.jsx
@@ -3,12 +3,14 @@ import { Router, Route, Link, hashHistory } from 'react-router'
 import Layout from '../components/Layout'
 import Portfolio from '../../portfolio'
 import Resume from '../../resume'
+import Contact from '../../contact'
 
 const Routes = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Layout} >
       <Route path='portfolio' component={Portfolio} />
       <Route path='resume' component={Resume} />
+      <Route path='contact' component={Contact} />
     </Route>
   </Router>
 )
---
+++ client/contact/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => (
+    <div className="content">
+      <a href="mailto:chelsea@bridson.me">
+        chelsea@bridson.me
+      </a>
+    </div>
+    )
+
+export default Contact
